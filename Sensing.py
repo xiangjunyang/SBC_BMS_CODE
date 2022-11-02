c50602aae321880cea48715a2de295c911800e75
xiangjunyang <robert8881837@gmail.com>
2022-11-02 15:09:25 +0000

    load model issue

--- Sensing.py
+++ Sensing.py
@@ -1,9 +1,57 @@
 import csv
 import json
 import serial
+import numpy as np
 from datetime import datetime
+from keras.models import load_model
 
 class Data():
+	def import_model(self):
+		self.SOC_Cha_model_files = './models/Cha_model.h5'
+		self.SOC_Discha_model_files = './models/Discha_model.h5'
+		self.SOH_Cha_model_files = './models/Cha_model.h5'
+		self.SOH_Discha_model_files = './models/Discha_model.h5'
+		self.SOC_Cha_model = load_model(SOC_Cha_model_files)
+		self.SOC_Discha_model = load_model(SOC_Discha_model_files)
+		self.SOH_Cha_model = load_model(SOH_Cha_model_files)
+		self.SOH_Discha_model = load_model(SOH_Discha_model_files)
+		print("load model success")
+
+	def Inverse_1D(inputs, lower_limit, upper_limit):
+		output = np.zeros(inputs.shape[0])
+		for i in range(inputs.shape[0]):
+			output[i] = (abs(inputs[i]-lower_limit)/abs(upper_limit-lower_limit))
+		return output
+
+
+	def Reverse_1D(inputs, lower_limit, upper_limit):
+		output = np.zeros(inputs.shape[0])
+		for i in range(inputs.shape[0]):
+			output[i] = ((inputs[i])*abs(upper_limit-lower_limit))+lower_limit
+		return output
+
+
+	def Inverse_Single_input(inputs, lower_limit, upper_limit):
+		output = (abs(inputs-lower_limit)/abs(upper_limit-lower_limit))
+		return output
+
+
+	def Reverse_Single_input(inputs, lower_limit, upper_limit):
+		output = (abs(upper_limit-lower_limit)*inputs)+lower_limit
+		return output
+
+
+	def cal_SOC(self,I, V, T, SOH):
+		V = Data.Inverse_Single_input(V, 2.3169, 2.7019)
+		T = Data.Inverse_Single_input(T, 25.7, 40.3)
+		SOH = Data.Inverse_Single_input(SOH, 97.0, 102.4)
+		if I > 0:
+			SOC_pred = self.SOC_Discha_model.predict((V, T, SOH))
+		else:
+			SOC_pred = self.SOC_Cha_model.predict((V, T, SOH))
+		SOC = Data.Reverse_Single_input(SOC_pred, 0, 100)
+		return SOC
+
 	def read_record_raw_data(Data_name):
 		#open csv file
 		BMS_file = open(Data_name, 'a+', newline='')
@@ -44,12 +92,18 @@ class Data():
 			response_json = stmserial.readline()
 			#print("get json data :\r\n",response_json)
 			decode = json.loads(response_json)
-			#print("decode json = ",decode)
+			print("decode json = ",decode)
+			V=decode["Stack_Voltage"]
+			I=decode["pack_current"]
+			Temp=decode["internalTemp"]
+			Pre_SOC = Data.cal_SOC(I, V, Temp, 100)
+			print("pre soc = ",Pre_SOC)
 			Current_Time = datetime.now()
 			writer.writerow([data_count_id, 'bms 1', decode["internalTemp"], decode["TS1Temp"], decode["TS3Temp"], decode["pack_current"], decode["Stack_Voltage"], decode["cell_voltage1"], decode["cell_voltage2"], decode["cell_voltage3"], decode["cell_voltage4"], decode["cell_voltage5"], decode["cell_voltage6"], decode["cell_voltage7"], decode["cell_voltage8"], decode["cell_voltage9"], decode["cell_voltage10"], decode["cell_voltage11"], decode["cell_voltage12"], decode["cell_voltage13"], decode["cell_voltage14"], decode["cell_voltage15"], decode["cell_voltage16"], decode["cell_voltage17"], decode["cell_voltage18"], decode["cell_voltage19"], decode["cell_voltage20"], 100, 100, Current_Time])
 			data_count_id += 1
 		BMS_file.close()
 def main():
+	Data.import_model()
 	Data.read_record_raw_data("./output_data/BMS1_output_data") 
 
 if __name__ == '__main__':
